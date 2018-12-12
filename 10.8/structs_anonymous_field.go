2c222d399eba0197b883f0609ad42125e231e635
郭漫丽 <404544332@qq.com>
2018-12-13 01:01:02 +0800

    setup

---
+++ 10.8/structs_anonymous_field.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type innerS struct {
+		in1 int
+		in2 int
+}
+
+type outerS struct {
+		b		int
+		c 	float32
+		int
+		innerS
+}
+
+func main() {
+		outer := new(outerS)
+		outer.b = 6
+		outer.c = 7.5
+		outer.int = 60
+		outer.in1 = 5
+		outer.in2 = 10
+
+		fmt.Printf("outer.int is %d\n", outer.int)		//针对每个结构体 对于每一种数据类型只能有一个匿名字段
+		fmt.Printf("outer.in1 is %d\n", outer.in1)
+		fmt.Printf("outer.in2 is %d\n", outer.in2)
+}
